ffe7879f6e5c4f827b4e895fc33ed223706b76a0
Efrain <riveraefrain5@gmail.com>
2020-07-14 15:07:21 -0600

    Config app

--- database/migrations/2020_02_23_190612_create_inventarios_table.php
+++ database/migrations/2020_02_23_190612_create_inventarios_table.php
@@ -15,7 +15,7 @@ class CreateInventariosTable extends Migration
     {
         Schema::create('inventarios', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('producto_id');
+            $table->unsignedBigInteger('producto_id');
             $table->integer('cantidad');
             $table->integer('tipo'); //Tipo se refiere a si es entrada o salida de productos. 1=Entrada 0=Salida
             $table->string('concepto');
